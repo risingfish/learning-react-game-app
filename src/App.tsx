392f6fd908a3c53308d1b321e8b7cb9398fdbb96
Zac f <risingfish@gmail.com>
2024-10-14 12:54:10 -0600

    Initial Layout for the main page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import { Button } from '@chakra-ui/react'
+import {Button, Grid, GridItem, Show} from '@chakra-ui/react'
 import './App.css'
 
 function App() {
-    return (<Button colorScheme='blue'>Button</Button>)
+    const breakpoints = {
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`
+    }
+    return (
+        <Grid templateAreas={breakpoints}>
+            <GridItem area={'nav'} bg='coral'>Nav</GridItem>
+            <Show above="lg">
+                <GridItem area={'aside'} bg='gold'>Aside</GridItem>
+            </Show>
+            <GridItem area={'main'} bg='dodgerblue'>Main</GridItem>
+        </Grid>
+    )
 }
 
 export default App
