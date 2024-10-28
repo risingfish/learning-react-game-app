e57f33c6da2003bde444d5ff9fb0bbfe13d6cce5
Zac f <risingfish@gmail.com>
2024-10-28 10:18:56 -0600

    Added GameCards and switched the list to a grid of GameCards

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Card, CardBody, Heading, Image} from '@chakra-ui/react'
+import Game from "../interfaces/Game";
+
+interface GameCardProps {
+    game: Game
+}
+
+function GameCard({ game }: GameCardProps) {
+    return (
+        <Card borderRadius={10} overflow="hidden">
+            <Image src={game.background_image} />
+            <CardBody>
+                <Heading fontSize='2xl'>{game.name}</Heading>
+            </CardBody>
+        </Card>
+    );
+}
+
+export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Game from "../interfaces/Game";
 import useGames from "../hooks/useGames";
+import {SimpleGrid} from "@chakra-ui/react";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
 
@@ -8,9 +10,9 @@ const GameGrid = () => {
     return (
         <>
             {error && <div>{error}</div>}
-            <ul>
-                {games.map(game => (<li key={game.id}>{game.name}</li>))}
-            </ul>
+            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} spacing={10}>
+                {games.map(game => (<GameCard key={game.id} game={game}></GameCard>))}
+            </SimpleGrid>
         </>
     )
 
