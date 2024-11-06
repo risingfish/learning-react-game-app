60f859381f343ba9dc6d367bc77ce5db7d94aef7
Zac f <risingfish@gmail.com>
2024-11-06 12:43:31 -0700

    Fixed spacing for platform dropdown.

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,46 @@
-import {Button, Grid, GridItem, Show} from '@chakra-ui/react'
-import './App.css'
+import { Button, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
+import "./App.css";
 import NavBar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
-import {Dispatch, SetStateAction, useState} from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import GenreList from "./components/GenreList";
 import Genre from "./interfaces/Genre";
 import PlatformSelector from "./components/PlatformSelector";
 
 function App() {
-    const breakpoints = {
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`
-    }
+  const breakpoints = {
+    base: `"nav" "main"`,
+    lg: `"nav nav" "aside main"`,
+  };
 
-    const columns = {
-        base: '1fr',
-        lg: '200px 1fr'
-    }
+  const columns = {
+    base: "1fr",
+    lg: "200px 1fr",
+  };
 
-    const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
 
-    return (
-        <Grid templateAreas={breakpoints} templateColumns={columns}>
-            <GridItem area={'nav'}>
-                <NavBar></NavBar>
-            </GridItem>
-            <Show above="lg">
-                <GridItem area='aside'>
-                    <GenreList onSelectGenre={setSelectedGenre} selectedGenre={selectedGenre} />
-                </GridItem>
-            </Show>
-            <GridItem area={'main'}>
-                <PlatformSelector></PlatformSelector>
-                <GameGrid selectedGenre={selectedGenre} />
-            </GridItem>
-        </Grid>
-    )
+  return (
+    <Grid templateAreas={breakpoints} templateColumns={columns}>
+      <GridItem area={"nav"}>
+        <NavBar></NavBar>
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside">
+          <GenreList
+            onSelectGenre={setSelectedGenre}
+            selectedGenre={selectedGenre}
+          />
+        </GridItem>
+      </Show>
+      <GridItem area={"main"}>
+        <Flex pb="3">
+          <PlatformSelector></PlatformSelector>
+        </Flex>
+        <GameGrid selectedGenre={selectedGenre} />
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
