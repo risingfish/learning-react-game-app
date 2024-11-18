a5dde2365a0fab2f41884d292af16f23deb0ec92
Zac f <risingfish@gmail.com>
2024-11-18 14:22:46 -0700

    Clean up game grid

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -3,9 +3,10 @@ import {Box} from "@chakra-ui/react";
 
 interface GameCardContainerProps {
     children: ReactNode,
+    key?: number
 }
 
-function GameCardContainer({children}: GameCardContainerProps) {
+function GameCardContainer({children, key}: GameCardContainerProps) {
     return (
         <Box borderRadius={10} overflow="hidden">
             {children}
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,7 +19,7 @@ const GameGrid = ({ gameQuery }: Props) => {
     return (
         <>
             {error && <div>{error}</div>}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={3}>
+            <SimpleGrid columns={{ sm: 1, md: 2, lg: 4 }} spacing={6}>
                 {isLoading &&
                     skeletons.map((skeleton) => (
                         <GameCardContainer key={skeleton}>
